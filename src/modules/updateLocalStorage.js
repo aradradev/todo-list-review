07061ba83c9b41f598cbcb38f094c7b64cbf7555
Abdoul <abdoulramanediallo44@gmail.com>
2023-06-13 14:33:55 +0000

    modules changed

--- src/modules/updateLocalStorage.js
+++
@@ -1,8 +0,0 @@
-// eslint-disable-next-line import/no-cycle
-import { tasks } from './renderTodoList.js';
-
-const updateLocalStorage = () => {
-  localStorage.setItem('tasks', JSON.stringify(tasks));
-};
-
-export default updateLocalStorage;
